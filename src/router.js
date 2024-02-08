f8d6c05a08ceeba6b55c87bd270fc4110da57712
Sergiyan92 <sergiyan@ukr.net>
2024-02-08 10:06:47 +0200

    update project

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Foo from "./pages/FooPage.vue";
-import Bar from "./pages/BarPage.vue";
+
 import HomePage from "./pages/HomePage.vue";
 import ApartmentPage from "./pages/ApartmentPage.vue";
 import Error from "./pages/ErrorPage.vue";
@@ -9,8 +8,6 @@ const routes = [
   { path: "/", component: HomePage, name: "home" },
   { path: "/apartments/:id", component: ApartmentPage, name: "apartment" },
   { path: "/:catchAll(.*)", component: Error, name: "error-page" },
-  { path: "/foo", component: Foo },
-  { path: "/bar", component: Bar },
 ];
 
 const router = createRouter({ history: createWebHistory(), routes });
