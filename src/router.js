6114aaa56ba211c1dbc05c52db9481f6986f0515
Sergiyan92 <sergiyan@ukr.net>
2024-02-27 09:15:40 +0200

    creta lazy load

--- src/router.js
+++ src/router.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import HomePage from "./pages/HomePage.vue";
-import ApartmentPage from "./pages/ApartmentPage.vue";
-import Error from "./pages/ErrorPage.vue";
-import Registration from "./pages/Registration.vue";
-import LoginPage from "./pages/LoginPage.vue";
-import MyOrders from "./pages/MyOrders.vue";
 import store from "./store/store";
+import Error from "./pages/ErrorPage.vue";
+
+const HomePage = () => import("./pages/HomePage.vue");
+const ApartmentPage = () => import("./pages/ApartmentPage.vue");
+const Registration = () => import("./pages/Registration.vue");
+const LoginPage = () => import("./pages/LoginPage.vue");
+const MyOrders = () => import("./pages/MyOrders.vue");
+
 const routes = [
   {
     path: "/",
